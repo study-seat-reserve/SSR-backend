746cdd4940e1eb99a1867afcfe5255c7529ab2b5
meme <00957051@mail.ntou.edu.tw>
2023-11-25 22:00:33 +0800

    refactor: remove Availability

--- src/model/timeslot.rs
+++ src/model/timeslot.rs
@@ -1,9 +1,8 @@
-use chrono::{NaiveDate, NaiveTime};
+use chrono::NaiveDate;
 
 #[derive(Debug)]
 pub struct TimeSlot {
   pub date: NaiveDate,
   pub start_time: u32,
   pub end_time: u32,
-  pub Availability: bool,
 }
