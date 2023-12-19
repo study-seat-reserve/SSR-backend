d94a4c7928101751ddb675a4cedb316e6bf4d0c8
meme <00957051@mail.ntou.edu.tw>
2023-12-19 23:51:01 +0800

    feat: add create_tables.sql for database setup

---
+++ create_tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS Seats (
+    seat_id INTEGER PRIMARY KEY,
+    available BOOLEAN NOT NULL,
+    other_info TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    user_role TEXT NOT NULL,
+    verified BOOLEAN NOT NULL,
+    verification_token TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Reservations (
+    user_name TEXT NOT NULL,
+    seat_id INTEGER NOT NULL,
+    start_time INTEGER NOT NULL,
+    end_time INTEGER NOT NULL,
+    PRIMARY KEY (user_name, start_time, end_time),
+    FOREIGN KEY(user_name) REFERENCES Users(user_name),
+    FOREIGN KEY(seat_id) REFERENCES Seats(seat_id)
+);
+
+CREATE TABLE IF NOT EXISTS UnavailableTimeSlots (
+    start_time INTEGER NOT NULL,
+    end_time INTEGER NOT NULL,
+    PRIMARY KEY (start_time, end_time)
+);
